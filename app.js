09de11a10711b3796c044dcf1aa7c964a6b8611e
sines210 <sines210@gmail.com>
2020-12-03 22:49:52 +0100

    version finie

--- app.js
+++ app.js
@@ -1,65 +1,79 @@
  // Créer un jeu simple de devinette de nombre. Le jeu choisit aléatoirement un nombre 
-// entre 1 et 100, // FAIT puis il met le joueur au défi de le deviner en 10 tentatives maxi //FAIT.À chaque tour, le 
-// joueur doit être informé s'il a deviné ou non le bon nombre // FAIT— si ce n'est pas le cas, le jeu lui 
-// indique si son estimation est trop basse ou trop élevée. //FAIT Le jeu se termine quand le joueur a deviné le nombre mystère, //FAIT ou 
-// s'il a épuisé ses 10 chances. //FAIT À la fin du jeu, le joueur a la possibilité de débuter une nouvelle 
-// partie. // A FAIRE
+// entre 1 et 100, puis il met le joueur au défi de le deviner en 10 tentatives maxi .À chaque tour, le 
+// joueur doit être informé s'il a deviné ou non le bon nombre si ce n'est pas le cas, le jeu lui 
+// indique si son estimation est trop basse ou trop élevée.  Le jeu se termine quand le joueur a deviné le nombre mystère,  ou 
+// s'il a épuisé ses 10 chances. À la fin du jeu, le joueur a la possibilité de débuter une nouvelle 
+// partie.
 //Bonus : Le jeu doit également rappeler au joueur les 
-// nombres déjà proposés // A FAIRE
+// nombres déjà proposés 
 
 
-
+var cl=console.log
 
     
 ///////////////////////////////////////////////////////////////////////
+
+ //Definition du nombre aléatoire, nombre généré aléatoirement entre 0 et 1, multiplié par 100 et arrondi//
 
     function getRandom() {
              var randomNumber =  Math.random() * 100;
             return Math.round(randomNumber) }
     getRandom()
+        
 
 ///////////////////////////////////////////////////////////////////////
-var myButton = document.querySelector('button')
-var randomNumber = getRandom()
+
+                
+
+//Définition des variables : je vais chercher mes éléments HTML, puis je créé une variable pour stocker mon nombre aléatoire (permet qu'il reste constant entre chaque essai) je créé un indice x à O qui permettra de compter le nombre de clics enfin je créé un tableau inputArray qui stockera les nombres déjà proposés. 
+
+
+var myButton = document.querySelector('.essai')
+var resetButton = document.querySelector('.reset')
 var myDivTest = document.querySelector('div')  
+var nbProposés = document.querySelector('p')
+var randomNumber = getRandom()
+var x = 0
+var inputArray = []
 
+//Fonctions du jeu qui démarre au clic
 
-var inputValue = () =>
+// Le compteur démarre avec  x+1 à chaque clic, puis la variable typedValue récupère la valeur tapée dans l'input, valeurs poussées dans le tableau inputArray et rajoutés dans le HTML des nombres proposés.
+// Les conditions renvoient des messages de félicitations, ou d'indication si la valeur est trop élevée ou trop basse (==, > ou <) les conditions prennent en compte le nombre de clic inférieur à 10. Si mes clics sont supérieurs à 10, une alert apparaitra pour indiquer qu'il faut recommencer la partie et le bouton reset va apparaitre (il était caché par un display:none)
+var randGame = () =>
 {
-  
- var typedValue = document.querySelector("input").value  
-
-if(typedValue == randomNumber)
+    myButton.addEventListener('click', () =>
+    {
+        x=x+1
+        var typedValue = document.querySelector('input').value
+        inputArray.push(`${typedValue}`)
+        // nbProposés.innerText=(`Nombres proposés : ${inputArray}`)
+        nbProposés.innerText="Nombres que vous avez proposés: " + inputArray
+     if(x<10 && typedValue == randomNumber)
 {myDivTest.innerText="bravo"
 window.alert(" Bravo !")}
-else if (typedValue> randomNumber)
-{ myDivTest.innerText=" Loupé trop haut. Rééssayez" + typedValue}
-else
-{myDivTest.innerText="Loupé trop bas. Rééssayez" + typedValue + randomNumber}
-
+else if (x<10 && typedValue> randomNumber)
+{ myDivTest.innerText=" Trop haut. Rééssayez"}
+else if(x<10  && typedValue < randomNumber)
+{myDivTest.innerText="Trop bas. Rééssayez" }
+else if(x>10)
+{window.alert(" Veuillez recommencer une partie")
+resetButton.style.display = 'block'
 }
+    })
+}
+randGame()
+
+//////////////////////////////////////////////////////////////////////////
+
+                //Bouton reset
+//RELOAD la page au clic
+
+resetButton.addEventListener('click', () =>{document.location.reload()} )
+
 
 ///////////////////////////////////////////////////////////////////////
 
-var x = 0
-var countClic = () =>
-{x = x+1; 
-    document.querySelector('p').innerHTML = x;
-if(x<10)
-{inputValue()}
-else
-{window.alert(" Veuillez rééssayer")
-}
-}
-
-///////////////////////////////////////////////////////////////////////
-
-var boutonClic = myButton.addEventListener("click", countClic)
 
 
 
-
-
-// TODO redémarrer la partie après les 10 tentatives + affichage valeurs précédentes + affichage des règles mise en forme ... +
-// Pour l'instant j'ai créé une fonction me permettant de générer un nombre aléatoire entre 0 et 1 et de le multiplier par 100 afin de générer un nombre aléatoire entre 1 et 100.
-// Ensuite j'ai créé une variable me permettant de stocker le résultat de ma fonction de génération de nombre aléatoire puisque si j'utilise ma fonction directement elle générera un nouveau nombre à chaque clique de l'utilisateur tandis que si je stocke la valeur générée dans une variable, la fonction n'est éxécutée qu'une fois et donc le nombre alétatoire est conservé et on peut le réutiliser pour les différents éssais. Ensuite j'ai créé une condition de comparaison qui affiche du texte pour l'utilisateur. 
